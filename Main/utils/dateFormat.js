55386d97101ae0f070ba00f87939909ff17a865b
Deshan <dcnm777@gmail.com>
2023-07-28 19:04:23 -0700

    cleaned up dateFormat.js

--- Main/utils/dateFormat.js
+++ Main/utils/dateFormat.js
@@ -1,17 +1,43 @@
+// Moved the month object outside of the function to avoid re-creation on every function call
+const months = {
+  0: 'Jan',
+  1: 'Feb',
+  2: 'Mar',
+  3: 'Apr',
+  4: 'May',
+  5: 'Jun',
+  6: 'Jul',
+  7: 'Aug',
+  8: 'Sep',
+  9: 'Oct',
+  10: 'Nov',
+  11: 'Dec',
+};
+
 const addDateSuffix = (date) => {
+  if (typeof date !== 'number') {
+    throw new Error('Date should be a number');
+  }
+
   let dateStr = date.toString();
 
   // get last char of date string
   const lastChar = dateStr.charAt(dateStr.length - 1);
 
-  if (lastChar === '1' && dateStr !== '11') {
-    dateStr = `${dateStr}st`;
-  } else if (lastChar === '2' && dateStr !== '12') {
-    dateStr = `${dateStr}nd`;
-  } else if (lastChar === '3' && dateStr !== '13') {
-    dateStr = `${dateStr}rd`;
-  } else {
-    dateStr = `${dateStr}th`;
+  // Add suffix based on last char
+  switch (lastChar) {
+    case '1':
+      dateStr += 'st';
+      break;
+    case '2':
+      dateStr += 'nd';
+      break;
+    case '3':
+      dateStr += 'rd';
+      break;
+    default:
+      dateStr += 'th';
+      break;
   }
 
   return dateStr;
@@ -22,21 +48,9 @@ module.exports = (
   timestamp,
   { monthLength = 'short', dateSuffix = true } = {}
 ) => {
-  // create month object
-  const months = {
-    0: monthLength === 'short' ? 'Jan' : 'January',
-    1: monthLength === 'short' ? 'Feb' : 'February',
-    2: monthLength === 'short' ? 'Mar' : 'March',
-    3: monthLength === 'short' ? 'Apr' : 'April',
-    4: monthLength === 'short' ? 'May' : 'May',
-    5: monthLength === 'short' ? 'Jun' : 'June',
-    6: monthLength === 'short' ? 'Jul' : 'July',
-    7: monthLength === 'short' ? 'Aug' : 'August',
-    8: monthLength === 'short' ? 'Sep' : 'September',
-    9: monthLength === 'short' ? 'Oct' : 'October',
-    10: monthLength === 'short' ? 'Nov' : 'November',
-    11: monthLength === 'short' ? 'Dec' : 'December',
-  };
+  if (isNaN(Date.parse(timestamp))) {
+    throw new Error('Invalid timestamp');
+  }
 
   const dateObj = new Date(timestamp);
   const formattedMonth = months[dateObj.getMonth()];
@@ -46,10 +60,7 @@ module.exports = (
     : dateObj.getDate();
 
   const year = dateObj.getFullYear();
-  let hour =
-    dateObj.getHours() > 12
-      ? Math.floor(dateObj.getHours() - 12)
-      : dateObj.getHours();
+  let hour = dateObj.getHours() % 12;
 
   // if hour is 0 (12:00am), change it to 12
   if (hour === 0) {
